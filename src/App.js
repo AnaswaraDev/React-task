3f0cc43cfdcf57ecd681d43224c54d5837c5613e
Sibiraj PR <sibiraj@lightrains.com>
2021-04-22 13:02:25 +0530

    Adding sample form

--- src/App.js
+++ src/App.js
@@ -2,48 +2,40 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import SignUp from './SignUp'
-import Home from './Home'
-import Sample from './Sample'
 import Homeone from './Homeone'
-import "tailwindcss/tailwind.css"
-import './index.css';
+import 'tailwindcss/tailwind.css'
+import './index.css'
 import Message from './Message'
 import Test2 from './Test2'
+import SampleForm from './SampleForm'
 
 function App() {
-  return (
-    <div className="container mx-auto">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/SignUp">
-            <SignUp />
-          </Route>
+    return (
+        <div className="container mx-auto">
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <SampleForm />
+                    </Route>
+                    <Route path="/SignUp">
+                        <SignUp />
+                    </Route>
 
-          <Route path="/Homeone">
-           <Homeone/>
-          </Route>
+                    <Route path="/Homeone">
+                        <Homeone />
+                    </Route>
 
-          <Route path="/Message">
-            <Message />
-          </Route>
+                    <Route path="/Message">
+                        <Message />
+                    </Route>
 
-          <Route path="/Sample">
-            <Sample />
-          </Route>
-          <Route path="/Test2">
-          <Test2 />
-        </Route>
-
-
-        </Switch>
-
-        
-      </Router>
-    </div>
-  )
+                    <Route path="/Test2">
+                        <Test2 />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
 export default App
